adcba9694aefde6756e9e795312bb316cd34028f
Pauline LE BARS <psalbre@gmail.com>
2025-06-04 19:48:44 +0200

    WIP page formulaire. HTML + CSS ok + changement de page dynamique avec JS

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -99,37 +99,76 @@ const addFilter = (filter) => {
 
 const token = sessionStorage.getItem("token");
 
+//If there's a Token
 if (token) {
   const edit = document.querySelector(".edit");
   const login = document.querySelector(".login");
   const logout = document.querySelector(".logout");
   const editBanner = document.querySelector(".edit-banner");
 
+  // display/hide the following
   filters.classList.add("hidden");
   edit.classList.remove("hidden");
   logout.classList.remove("hidden");
   login.classList.add("hidden");
   editBanner.classList.remove("hidden");
 
+  //when we want to log out, remove the token
   logout.addEventListener("click", () => {
     sessionStorage.removeItem("token");
     window.location.reload();
   });
 
+  // when we click the edit button
   edit.addEventListener("click", () => {
     const exit = document.querySelector(".exit");
     const modal = document.querySelector(".modal");
+    const modalBtn = document.querySelector(".modal-button");
 
+    // a modal is displayed
     editModal.classList.replace("hidden", "logged");
 
+    // hide the modal when we click the exit button
     exit.addEventListener("click", () => {
       editModal.classList.replace("logged", "hidden");
     });
 
+    // also hide the modal when we click outside the modal window
     modal.addEventListener("click", (event) => {
       if (event.target.classList.contains("modal")) {
         editModal.classList.replace("logged", "hidden");
       }
+    });
+
+    // when we click the button "ajouter des photos"
+    modalBtn.addEventListener("click", () => {
+      const titleModal = document.querySelector("#titlemodal");
+      const modalGallery = document.querySelector(".modal-gallery");
+      const returnIcon = document.querySelector(".return");
+      const secondTitle = document.querySelector(".add-photo-title");
+      const imgUploadForm = document.querySelector(".upload-photos");
+      const label = document.querySelector(".image-label");
+
+      // hide the gallery page and display the form page
+      returnIcon.classList.remove("hidden");
+      secondTitle.classList.replace("hidden", "page-2");
+      imgUploadForm.classList.replace("hidden", "page-2");
+      label.classList.replace("hidden", "page-2");
+      titleModal.classList.add("hidden");
+      modalGallery.classList.replace("page-1", "hidden");
+      modalBtn.classList.replace("display-btn", "hidden");
+
+      //When we click the left arrow
+      returnIcon.addEventListener("click", () => {
+        //Go back to the gallery page
+        returnIcon.classList.add("hidden");
+        secondTitle.classList.replace("page-2", "hidden");
+        imgUploadForm.classList.replace("page-2", "hidden");
+        label.classList.replace("page-2", "hidden");
+        titleModal.classList.remove("hidden");
+        modalGallery.classList.replace("hidden", "page-1");
+        modalBtn.classList.replace("hidden", "display-btn");
+      });
     });
   });
 }
