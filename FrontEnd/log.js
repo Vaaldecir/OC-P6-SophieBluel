7ea1f5f71685697503d6294e509f10fd1dcd3c26
Pauline LE BARS <psalbre@gmail.com>
2025-06-11 18:49:26 +0200

    Ajout de commentaires fichiers JS + WIP formulaire d'envoi des works (ajout de l'appel API pour les catégories)

--- FrontEnd/log.js
+++ FrontEnd/log.js
@@ -1,13 +1,14 @@
+// @ts-nocheck
 const form = document.querySelector("form");
-console.log(form);
 
-// @ts-ignore
+//When the user submit its IDs
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  // @ts-ignore
+
   const formData = new FormData(form);
   const email = formData.get("email");
   const password = formData.get("password");
+  // Go search for the right IDs in the API
   fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
@@ -20,12 +21,15 @@ form.addEventListener("submit", (event) => {
     }),
   })
     .then((response) => {
+      // if the mail and/or password doesn't match
       if (!response.ok) {
+        // Alert the user that there's a problem
         alert("Utilisateur inconnu");
       }
       return response.json();
     })
     .then((data) => {
+      // if the IDs match, then create a token and go back to the main page
       console.log(data);
       sessionStorage.setItem("token", data.token);
       window.location.href = "index.html";
